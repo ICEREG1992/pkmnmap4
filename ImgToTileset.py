efb5e76a27a0620518593ad66999bafbcab9ef56
William Sullivan <willwill.sullivan@gmail.com>
2024-03-25 00:50:26 -0500

    update tileset generator and documentation

---
+++ ImgToTileset.py
@@ -0,0 +1,59 @@
+import shutil, os
+from wand.image import Image
+def ImageToTileset(source, dest):
+    print("Exporting multiple sizes of your source image...")
+    os.mkdir(dest+'\\maps')
+    ImageToSizes(source, dest)
+    sizes = os.listdir(dest+'\\maps')
+    os.mkdir(dest+'\\grid')
+    print("Converting each size into a Tileset...")
+    for i in sizes:
+        size = len(sizes) - int(i[:i.rfind('.')]) + 1
+        print("Creating grid for zoom level " + str(size))
+        width = ImageToGrid(dest+'\\maps\\'+i, dest+'\\grid\\') #this is jank but we need to get width to GridToTileset
+        os.mkdir(dest+'\\'+str(size))
+        print("Arranging grid into directory")
+        GridToTileset(dest+'\\grid', dest+'\\'+str(size), int(width/256))
+    print("Done!")
+
+# explort multiple sizes of a source image
+def ImageToSizes(source, dest):
+    with Image(filename=source) as img:
+        dim = (img.size[0] * 2, img.size[1] * 2)
+        ind = 0
+        while (dim[0] % 256 == 0 and dim[1] % 256 == 0):
+            print("Exporting size " + str(ind))
+            #export each smaller version
+            with img.clone() as i:
+                i.resize(dim[0], dim[1], filter='point')
+                i.save(filename=dest+'\\maps\\'+str(ind)+'.png')
+                pass
+            dim = (int(dim[0]/2), int(dim[1]/2))
+            ind = ind + 1
+
+# split image into 256x256 sub-images
+def ImageToGrid(source, dest):
+    with Image(filename=source) as img:
+        i = 0
+        for h in range(0, img.size[1], 256):
+            for w in range(0, img.size[0], 256):
+                w_end = w + 256
+                h_end = h + 256
+                with img[w:w_end, h:h_end] as tile:
+                    tile.save(filename=dest+'{:06d}.png'.format(i))
+                i = i + 1
+        return img.size[0]
+
+# arrange a folder of grid images into a tileset
+def GridToTileset(source, dest, width):
+    files = os.listdir(source)
+    ptr = 0
+    ptr2 = 0
+    os.mkdir(dest+'\\'+str(ptr2))
+    for f in files:
+        shutil.move(source+'\\'+f, dest+'\\'+str(ptr2)+'\\'+str(ptr)+'.png')
+        ptr = ptr + 1
+        ptr = ptr % width
+        if ptr == 0:
+            ptr2 = ptr2 + 1
+            os.mkdir(dest+'\\'+str(ptr2))
--- README.md
+++ README.md
@@ -2,13 +2,16 @@
 A rip of <https://pkmnmap.com> updated to Generation 4 of the Pokémon series. Uses [Leaflet](https://leafletjs.com/) to generate an interactive map.
 
 # how to generate a new tileset
-1. procure a high-resolution map of whatever you want to make a tileset out of. make sure the dimensions of the map image are very square, don't worry if you have to add a lot of whitespace
-1. export the map image repeatedly, halving resolution each time
-1. generate one final map with double resolution
+1. Procure a high-resolution map of whatever you want to make a tileset out of. make sure the dimensions of the map image are easily halved, don't worry if you have to add a lot of whitespace
+1. I've included my personal script for converting a large png into a tileset with multiple zoom levels in this respository as ImgToTileset.py. It will resize your map to the various zoom levels, and for each resize it will split the image into 256x256 pixel tiles which then get arranged into the proper directory structure for Leaflet to be able to render your map.
 1. install [ImageMagick](https://imagemagick.org/index.php)
-1. for each exported map image, run the following, where A and B are the image's dimensions divided by 256:
-```magick convert "[map path]" -crop AxB@ +repage +adjoin "[temp path]"```
-1. this will slice your map image into the tiles that you need for your tile set. now you need to assemble them into a directory. you can do this with the "rename" function included in this repository. run the following:
-```from rename import rename
-rename(r'[temp path]', r'[tileset path for this zoom level]', [how many tiles wide your image is])```
-1. now you have a set of images for one zoom level. repeat the previous two steps for as many zoom levels as you have.
+1. install prerequisites for ImgToTileset.py:
+    -shutil
+    -os
+    -wand
+1. Run the following in your favorite Python interpreter:
+```
+from ImgToTileset import ImageToTileset
+ImageToTileset(r"[path to your png map]", r"[directory you want the tileset to be placed]")
+```
+1. To see your new map, you can update Overworld.js to point to the tileset directory, just make sure to change `tileX` and `tileY` in main.js to be representative of your map's aspect ratio.
