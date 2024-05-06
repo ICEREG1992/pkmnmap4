b15441b42610ac6dad835dc6db54213e3d56cb60
William Sullivan <willwill.sullivan@gmail.com>
2024-05-06 00:36:17 -0500

    remove node, use module references

--- public/firebaseAnalytics.js
+++ public/firebaseAnalytics.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-analytics.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyDHl8BHznN6jOIDScX8_DX7e_IGrE_775w",
